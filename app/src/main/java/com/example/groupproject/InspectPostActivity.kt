1bd37087d4b9078e17478a420c458daeef98c7cb
viveroa2291 <viveroa2291@uwec.edu>
2021-11-08 14:47:29 -0600

    Added the Inspect Page and committing since I got an alert on my computer something about out of memory. Saving these commits here. I am working on some alignments and background colors. Almost finished.

---
+++ app/src/main/java/com/example/groupproject/InspectPostActivity.kt
@@ -0,0 +1,11 @@
+package com.example.groupproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InspectPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inspect_post)
+    }
+}
--- app/src/main/java/com/example/groupproject/PostActivity.kt
+++ app/src/main/java/com/example/groupproject/PostActivity.kt
@@ -2,6 +2,7 @@ package com.example.groupproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.core.text.HtmlCompat
 
 class PostActivity : AppCompatActivity() {
@@ -9,10 +10,14 @@ class PostActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+        // I want to have an <hr> tag below the location and description.
+        val locationTextView: TextView = findViewById(R.id.locationView)
+        val locationText = getString(R.string.location_of_the_image)
+        val tagHandler = HtmlTagHandler()
+        locationTextView.text = HtmlCompat.fromHtml(locationText, HtmlCompat.FROM_HTML_MODE_LEGACY, null, tagHandler)
         /*
         val html = "Hello <br> hai<br> I am fine <hr> Another line here <hr><hr>"
         val tagHandler = HtmlTagHandler()
-
 
         textView.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_LEGACY, null, tagHandler)
          */
