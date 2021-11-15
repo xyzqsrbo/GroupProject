e2d865ba2578077c272bf5074e9fdc96113cf3a1
viveroa2291 <viveroa2291@uwec.edu>
2021-11-15 17:57:45 -0600

    I got my upload image to work. What I plan to do next after our meeting is to make the post, cancel, and draft button do what they're suppose to do and then jump to the inspect page and finish up there.

--- app/src/main/java/com/example/groupproject/PostActivity.kt
+++ app/src/main/java/com/example/groupproject/PostActivity.kt
@@ -1,15 +1,36 @@
 package com.example.groupproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 
 class PostActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+    private lateinit var editText: EditText
+
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        button = findViewById(R.id.uploadImageButton)
+        imageView = findViewById(R.id.addImage)
+        editText = findViewById(R.id.description)
+
+        button.setOnClickListener{
+            pickImageGallery()
+        }
+
         // I want to have an <hr> tag below the location and description.
         val locationTextView: TextView = findViewById(R.id.locationView)
         val locationText = getString(R.string.location_of_the_image)
@@ -21,6 +42,20 @@ class PostActivity : AppCompatActivity() {
 
         textView.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_LEGACY, null, tagHandler)
          */
+    }
+    private fun pickImageGallery()
+    {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            imageView.setImageURI(data?.data)
+            button.setY(225F) // This will move the button down below the image so it is not overlapping it.
+        }
     }
 
 }
