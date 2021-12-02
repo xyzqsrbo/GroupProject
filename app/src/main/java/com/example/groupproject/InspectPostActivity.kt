6ef4a27fb485f7de4ef12f298a59e28d3baad22d
viveroa2291 <viveroa2291@uwec.edu>
2021-12-02 02:34:22 -0600

    Made some updates on the inspect page, however there are still some bugs that I need to fix.

--- app/src/main/java/com/example/groupproject/InspectPostActivity.kt
+++ app/src/main/java/com/example/groupproject/InspectPostActivity.kt
@@ -2,10 +2,32 @@ package com.example.groupproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class InspectPostActivity : AppCompatActivity() {
+    var likeCounter = 0
+    var dislikeCounter = 0
+    private lateinit var likeButton: Button
+    private lateinit var dislikeButton: Button
+    private lateinit var likeIncrementer: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inspect_post)
+
+        likeButton = findViewById(R.id.likeButton)
+        likeIncrementer = findViewById(R.id.likeIncrementer)
+        dislikeButton = findViewById(R.id.dislikeButton)
+
+        likeButton.setOnClickListener {
+            likeCounter++
+            likeIncrementer.setText("$likeCounter")
+        }
+        /*
+        dislikeButton.setOnClickListener {
+            dislikeCounter++
+        }
+         */
     }
 }
